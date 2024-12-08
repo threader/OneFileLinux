dd5c50fc2d62d7f517e9f23b327fcc36e3cf0bf3
threader <michael.heltne@gmail.com>
2024-12-09 00:51:20 +0100

    buildroot: config: add -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE and always -D_GLIBCXX_ASSERTIONS https://libcxx.llvm.org/Hardening.html https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++.html#precondition-checks-for-c-standard-library-calls https://security.googleblog.com/2024/11/retrofitting-spatial-safety-to-hundreds.html
    buildroot: configs: i need to double check everything again i guess

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ ! -e linux/.git ]; then
+if [ ! -d linux/.git ]; then
+# ln -s $PWD/buildroot/output/build/linux-main_ofl/ $PWD/linux
  ln -s $PWD/buildroot/dl/linux/git $PWD/linux/.git
 fi
 
