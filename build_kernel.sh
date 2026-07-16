e36f3631cf52935415602e3adc5bd1b22e68e90c
threader <michael.heltne@gmail.com>
2026-07-16 22:54:01 +0200

    redo linux git stuff

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-DPKG_BUILD=´which dpkg-buildpackage´
+OLD_PWD=$PWD
+DPKG_BUILD='which dpkg-buildpackage'
 # if [ -f $DPKG_BUILD ]; then
-ln -s $PWD/debian/debian/ $PWD/linux/debian
-# fi
 
-ROOT_ARCH=´uname -m´
+if [ ! -d $PWD/linux/debian ]; then
+ln -s $PWD/debian/debian $PWD/linux/debian
+fi
+
+if [ ! -d $PWD/linux/.git ]; then
+ln -s $PWD/buildroot/dl/linux/git/.git $PWD/linux/.git
+fi
+
+#ROOT_ARCH=`uname -m`
 cd linux; git fetch; git reset --hard remotes/origin/linux-rolling-stable
 # git gc --aggressive --prune=all
 
 #if [ -f $DPKG_BUILD ]; then
-cd debian; git fetch; git reset --hard debian/latest
-patch -p1 < ../../patches/0000_dont_clean_kernel_build_on_error.patch; 
+cd linux/debian; git fetch; git reset --hard debian/latest
+patch -p1 < $PWD/patches/0000_dont_clean_kernel_build_on_error.patch; 
 # git gc --aggressive --prune=all
 cd ..
 #fi
@@ -18,14 +25,16 @@ cd ..
 # Apply Debian patches regardless 
 make -f debian/rules source;
 # Configure build for the current the running pc/perhapsials 
-make localmodconfig
-../kernel-hardening-checker/bin/kernel-hardening-checker -g ${ROOT_ARCH^^} > .config-harden-stub
-./scripts/kconfig/merge_config.sh .config .config-harden-stub
-../kernel-hardening-checker/bin/kernel-hardening-checker -c .config
-cp .config ../cfg/current_building_kernel_config
-yes ´´ | make menuconfig
+#make localmodconfig; 
+make oldconfig
+$PWD/kernel-hardening-checker/bin/kernel-hardening-checker -g X86_64 > $PWD/cfg/config-harden-stub
+$PWD/linux/scripts/kconfig/merge_config.sh $PWD/.config $PWD/cfg/config-harden-stub
+$PWD/kernel-hardening-checker/bin/kernel-hardening-checker -c $PWD/linux/.config
+cp $PWD/linux/.config $PWD/cfg/current_building_kernel_config
+# yes ´´ | make menuconfig
 
-if [ -f $DPKG_BUILD ]; then
+#if [ -f $DPKG_BUILD ]; then
 # dpkg-buildpackage -us -ui -uc --no-sign --build=binary --no-post-clean
-fi
+#fi
 
+cd $OLD_PWD/buildroot; make # ./build_buildroot.sh
