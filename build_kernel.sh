413745b853c0f95531d45f23ae779e5a6f5644f0
threader <michael.heltne@gmail.com>
2026-07-16 16:39:34 +0200

    Try, try and try again to get this working properly

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,21 +3,29 @@ DPKG_BUILD=´which dpkg-buildpackage´
 # if [ -f $DPKG_BUILD ]; then
 ln -s $PWD/debian/debian/ $PWD/linux/debian
 # fi
+
 ROOT_ARCH=´uname -m´
 cd linux; git fetch; git reset --hard remotes/origin/linux-rolling-stable
-#git gc --aggressive --prune=all
+# git gc --aggressive --prune=all
 
-# Apply Debian patches. 
-# if [ -f $DPKG_BUILD ]; then
-cd debian; patch -p1 < ../../patches/0000_dont_clean_kernel_build_on_error.patch; cd ..
-make -f debian/rules source; 
-# fi
+#if [ -f $DPKG_BUILD ]; then
+cd debian; git fetch; git reset --hard debian/latest
+patch -p1 < ../../patches/0000_dont_clean_kernel_build_on_error.patch; 
+# git gc --aggressive --prune=all
+cd ..
+#fi
 
-# Grab the running configuration of loaded modules as the base.
+# Apply Debian patches regardless 
+make -f debian/rules source;
+# Configure build for the current the running pc/perhapsials 
 make localmodconfig
 ../kernel-hardening-checker/bin/kernel-hardening-checker -g ${ROOT_ARCH^^} > .config-harden-stub
 ./scripts/kconfig/merge_config.sh .config .config-harden-stub
 ../kernel-hardening-checker/bin/kernel-hardening-checker -c .config
+cp .config ../cfg/current_building_kernel_config
+yes ´´ | make menuconfig
+
 if [ -f $DPKG_BUILD ]; then
 # dpkg-buildpackage -us -ui -uc --no-sign --build=binary --no-post-clean
 fi
+
